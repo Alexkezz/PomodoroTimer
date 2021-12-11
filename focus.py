ddc3f953e85d7f59fd3b6df764132b6de2f57e0f
Alexkezz <alex391969@gmail.com>
2021-12-11 23:23:40 +0100

    Perfect

--- focus.py
+++ focus.py
@@ -10,8 +10,8 @@ mi_frame = Frame(root)
 mi_frame.pack()
 mi_frame.config(bg="red")
 
-texto = Label(mi_frame, text="Introduce el tiempo que quieres trabajar", font=("Calibri", 20), bg="grey")
-texto.grid(row=0, column=0, columnspan=3, pady=(10,20), padx=20)
+texto = Label(mi_frame, text="Introduce el tiempo que quieres trabajar", font=("Calibri", 30), bg="grey")
+texto.grid(row=0, column=0, columnspan=3, pady=(0, 30))
 
 numero_pantalla_minutos = StringVar()
 numero_pantalla_segundos = StringVar()
@@ -162,12 +162,12 @@ boton_subir.grid(row=1, column=0, columnspan=1, sticky="e")
 foto = PhotoImage(file="abajo.png")
 foto = foto.subsample(80)
 boton_bajar = Button(mi_frame, image=foto, width=100, height=50,bg="grey", command=lambda:pulsar_abajo(pantalla_minutos.get()))
-boton_bajar.grid(row=3, column=0, columnspan=1, sticky="e", pady=(0, 100))
+boton_bajar.grid(row=3, column=0, columnspan=1, sticky="e", pady=(0, 130))
 
 boton_empezar = Button(mi_frame, text="EMPEZAR", font=("Calibri", 20), bg="grey", command=lambda:start1())
-boton_empezar.place(x=90, y=340)
+boton_empezar.place(x=190, y=370)
 
 boton_pausar = Button(mi_frame, text="PAUSAR", font=("Calibri", 20), bg="grey", command=lambda:pausar())
-boton_pausar.place(x=260, y=340)
+boton_pausar.place(x=360, y=370)
 
 root.mainloop()
